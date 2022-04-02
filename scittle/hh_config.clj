fc6c35f96293d10d31a18508396ec6494738fb75
richie <6143833+rgkirch@users.noreply.github.com>
2022-04-02 16:55:12 -0400

    demo that adds rows

--- scittle/hh_config.clj
+++ scittle/hh_config.clj
@@ -28,25 +28,52 @@
                         :src "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"}]
               [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.1.2/dist/scittle.reagent.js"
                         :type "application/javascript"}]
+              (hiccup.util/raw-string "<script type=\"module\" src=\"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js\"></script>")
+              (hiccup.util/raw-string "<script nomodule src=\"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js\"></script>")
               (scittle
                (require '[reagent.core :as r]
                         '[reagent.dom :as rdom])
-               (def state (r/atom {:clicks 0}))
+
+               (def state (r/atom [0]))
+
                (defn my-component []
-                 [:div
-                  [:p "Clicks: " (:clicks @state)]
-                  [:button.button {:on-click #(swap! state update :clicks inc)}
-                   "Click me!"]])
+                 [:div {:style {:display "flex"
+                                :flex-direction "column"}}
+                  (doall
+                   (for [[ix clicks] (keep-indexed vector @state)]
+                     [:div {:style {:display "flex"}}
+                      [:p {:style {:font-size "x-large"}}
+                       "Clicks: " [:span.tag.is-large clicks]]
+                      [:div {:style {:flex "1"}}]
+                      [:button.button
+                       {:on-click #(swap! state update ix dec)}
+                       [:ion-icon {:name "remove-outline"
+                                   :size "large"}]]
+                      [:button.button.is-link
+                       {:on-click #(swap! state update ix inc)}
+                       [:ion-icon {:name "add-outline"
+                                   :size "large"}]]]))
+                  [:button.button.is-primary
+                   {:on-click #(swap! state conj 0)}
+                   [:ion-icon {:name "add-circle-outline"
+                               :size "large"}]]])
 
                (rdom/render [my-component] (.getElementById js/document "app")))
               [:title "Hacker-Hud Conf"]]
              [:body
-              [:div#app]]]))
+              [:div#app]
+              ;; (hiccup.util/raw-string "<script type=\"module\" src=\"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js\"></script>")
+              ;; (hiccup.util/raw-string "<script nomodule src=\"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js\"></script>")
+              ;; [:script {:type "module"
+              ;;           :src "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"}]
+              ;; [:script (hiccup.util/raw-string "nomodule") {:src "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"}]
+              ]]))
 
 (defn spit-html
   []
   (spit "index.html" html)
-  (shell "tidy -modify -indent -wrap 120 index.html"))
+  (try (shell "tidy -modify -indent -wrap 120 index.html")
+       (catch Exception e (println e))))
 
 (defn -main
   []
